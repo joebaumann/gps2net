f6a4f626dc9ef727580ed7126c2d72f351a99be0
joehonig <joachimbaumann1@gmail.com>
2019-12-05 15:03:27 +0100

    makes sure that the _pycache_ folder is not created

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 import sphinx_rtd_theme
 import os
 import sys
+# makes sure that the _pycache_ folder is not created
+sys.dont_write_bytecode = True
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -63,5 +65,6 @@ html_theme = 'bizstyle'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
+html_static_path = []
 
